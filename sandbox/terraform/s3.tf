2f5fe525fccf705fe9cfb32f09cca786a45ce0f9
cyrildmoses <cyrild.moses@gmail.com>
2024-08-03 06:36:18 -0400

    new resoruce

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,7 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_sns_topic" "example" {
+  name = "my-sns-topic-new-account"
+}
