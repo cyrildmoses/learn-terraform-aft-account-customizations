8a6c3fbaffbefbb8f07262fb272167b3bc0ac2e2
cyrildmoses <cyrild.moses@gmail.com>
2024-08-03 03:59:26 -0400

    prod customization

---
+++ workload-prod/terraform/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+resource "aws_sns_topic" "example" {
+  name = "my-sns-topic-new-account"
+}
